45e0af58b13ad6e425042e6c0a6c2bf94659ec8c
DaNnyKg07 <parasyadavnirban999@gmail.com>
2021-02-06 15:32:04 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/mydiceee/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mydiceee
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              'Dicee',
+            ),
+          ),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDice = 1;
+  int rightDice = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  //setState is used to run the build method again and see what changes are made and rebuild the changes on the screen.
+                  //basically it is used to change the state and rebuild it.
+                  leftDice = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset('images/dice$leftDice.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  //setState is used to run the build method again and see what changes are made and rebuild the changes on the screen.
+                  //basically it is used to change the state and rebuild it.
+                  rightDice = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset('images/dice$rightDice.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
